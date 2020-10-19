72b9e4d19e83fb63b263666796999e20b8f265a8
ca3rot <ca3rot@gmail.com>
2020-10-19 16:36:12 +0900

    commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  title: 'VuePress 시작!',
+  description: 'Hello World',
+  themeConfig: {
+      nav: [
+      { text: 'Home', link: '/' },
+      { text: 'About', link: '/about/' }
+    ]
+  }
+}
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,3 @@
-# Hello Vuepress
+# Today I Learned
+
+이 페이지에서 제가 배운 것들을 이야기 하겠습니다.
