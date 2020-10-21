ac9b982864352014651165a47fcabf430fa0bc36
ca3rot <ca3rot@gmail.com>
2020-10-21 20:48:25 +0900

    test

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,26 @@ module.exports = {
   themeConfig: {
       nav: [
       { text: 'Home', link: '/' },
-      { text: 'About', link: '/about/' }
+      { text: 'About', link: '/about/' },
+      {
+        text: 'Languages',
+        ariaLabel: 'Language Menu',
+        items: [
+          { text: 'Chinese', link: '/language/chinese/' },
+          { text: 'Japanese', link: '/language/japanese/' }
+        ]
+      }
     ]
-  }
+  },
+  plugins: [['@vuepress/back-to-top'], ['@vuepress/plugin-active-header-links'], 
+      ['@vuepress/last-updated',
+      {
+        transformer: (timestamp, lang) => {
+          // Don't forget to install moment yourself
+          const moment = require('moment')
+          moment.locale(lang)
+          return moment(timestamp).fromNow()
+        }
+      }]
+  ]
 }
