96ef67902da4b80fac2a730c4eb0adb6c71a024b
ca3rot <ca3rot@gmail.com>
2020-10-22 13:14:24 +0900

    commit all

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,28 +1,45 @@
 module.exports = {
   title: 'Today Nara Learned',
-  description: 'Hello World',
+  description: 'TIL pages',
+  head: [
+    ['link', { rel: 'icon', href: '/logo.jpg' }]
+  ],
   themeConfig: {
       nav: [
       { text: 'Home', link: '/' },
       { text: 'About', link: '/about/' },
       {
-        text: 'Languages',
-        ariaLabel: 'Language Menu',
+        text: 'Cloud',
+        ariaLabel: 'Cloud',
         items: [
-          { text: 'Chinese', link: '/language/chinese/' },
-          { text: 'Japanese', link: '/language/japanese/' }
+          { text: 'AWS', link: '/cloud/aws/' },
+        ]
+      },
+      {
+        text: 'Code',
+        ariaLabel: 'code',
+        items: [
+          { text: 'Javascript', link: '/code/javascript/' },
+          // { text: 'React', link: '/code/react' }
+        ]
+      },
+      {
+        text: 'Challenge',
+        ariaLabel: 'challenge',
+        items: [
+          { text: 'nodebird', link: '/challenge/nodebird/' },
         ]
       }
     ]
   },
-  plugins: [['@vuepress/back-to-top'], ['@vuepress/plugin-active-header-links'], 
+  plugins: [['@vuepress/back-to-top'], 
+      ['@vuepress/plugin-active-header-links'], 
       ['@vuepress/last-updated',
       {
         transformer: (timestamp, lang) => {
-          // Don't forget to install moment yourself
           const moment = require('moment')
           moment.locale(lang)
-          return moment(timestamp).fromNow()
+          return moment(timestamp).format("YYYY/M/D h:m:s A")
         }
       }]
   ]
