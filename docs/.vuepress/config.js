7c2f9213025791d392579b07f1d777ad535f9f79
ca3rot <ca3rot@gmail.com>
2021-08-09 19:46:39 +0900

    commit

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -68,4 +68,16 @@ module.exports = {
       },
     },
   },
+  chainWebpack: (config, isServer) => {
+    config.module
+      .rule('vue')
+      .uses.store.get('vue-loader')
+      .store.get('options').transformAssetUrls = {
+      video: ['src', 'poster'],
+      source: 'src',
+      img: 'src',
+      image: ['xlink:href', 'href'],
+      a: 'href',
+    };
+  },
 };
