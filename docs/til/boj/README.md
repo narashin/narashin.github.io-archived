6269e418238c8fa0725783a6a17889f403612d76
ca3rot <ca3rot@gmail.com>
2021-08-22 09:47:11 +0900

    commit

--- docs/til/boj/README.md
+++
@@ -1,7 +0,0 @@
----
-title: BOJ
-description: BOJ
-sidebarDepth: 2
----
-
-제가 푼 문제들을 설명합니다.
--- docs/til/boj/boj.js
+++
@@ -1,9 +0,0 @@
-let input = 5;
-
-for (var i = 0; i < input; i++) {
-  let star = '';
-  for (var j = input - 1; j >= 0; j--) {
-    star += j <= i ? '*' : ' ';
-  }
-  console.log(star);
-}
